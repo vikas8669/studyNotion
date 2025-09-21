4fc46652b789edc2001e26d95c547adbda516fef
vikas kumar <vikayadav8669@gmail.com>
2025-09-22 01:23:44 +0530

    jgkdbvjd

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json()); // to parse json body
 app.use(cookieParser());
 app.use(
     cors({
-        // origin: 'http://localhost:5173', // frontend link
+        origin: 'https://study-notion-one-henna.vercel.app', // frontend link
         origin: "*",
         credentials: true
     })
