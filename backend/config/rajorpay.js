eb09ecfab491ed34ca5297f6c120aed78524171a
vikas kumar <vikayadav8669@gmail.com>
2025-09-22 00:40:38 +0530

    new

---
+++ backend/config/rajorpay.js
@@ -0,0 +1,9 @@
+const Rajorpay = require('razorpay');
+require('dotenv').config();
+
+
+require('dotenv').config();
+exports.instance = new Rajorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET
+})
